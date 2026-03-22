a8a8eced8b8bcb35d9d30ce8239560b8a0a4b04c
elizaASilva <elizabeth.asilvaalves@gmail.com>
2026-03-22 16:11:35 -0300

    Subindo projeto AgroConnect

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Cadastro from "./assets/Cadastro/cadastro";
 import PainelAgricultor from "./assets/PainelAgricultor/PainelAgricultor";
 import Familia from "./assets/Familia/Familia";
 
-function App() {n
+function App() {
   const [currentPage, setCurrentPage] = useState('home');
 
   return (
